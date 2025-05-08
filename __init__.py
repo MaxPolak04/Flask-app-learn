6d05b58dd28fb3f3c6686691354c9a7c20e69ac7
Maksymilian Polak <max.polak04@gmail.com>
2025-05-08 18:34:31 +0200

    Trying create SQLAlchemy Database

--- __init__.py
+++ __init__.py
@@ -1,8 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///data/users.db'
+
+    db.init_app(app)
+
+    # with app.app_context():
+    #     print("Tworzę bazę danych...")
+    #     db.create_all()
+    #     print("Baza danych powinna być gotowa!")
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -11,4 +33,3 @@ def create_app():
     app.register_blueprint(auth_blueprint)
 
     return app
-
---
+++ models.py
@@ -0,0 +1,16 @@
+from . import db
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     email = db.Column(db.String(100), unique=True)
+#     password = db.Column(db.String(100))
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
